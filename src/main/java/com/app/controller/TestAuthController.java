b5cc32046ceb00aa96de5efb4c903034e698d4e9
cursoSpring2021 <dguerra723@unab.edu.co>
2024-10-20 20:02:18 -0500

    Se agregaron dos clases TestAuthController y hello-Secured

---
+++ src/main/java/com/app/controller/TestAuthController.java
@@ -0,0 +1,19 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class TestAuthController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+
+    @GetMapping("/hello-secured")
+    public String helloSecured() {
+        return "Hello world Secured";
+    }
+}
